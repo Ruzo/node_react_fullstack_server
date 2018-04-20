d250dd21e66a670e4e2b6c157d049e5d7a32fd84
Richard Rouzeau <richard@evigy.com>
2018-04-20 19:46:42 -0400

    No idea how to fix api yet

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const cookieSession = require( 'cookie-session' );
 const passport = require( 'passport' );
 const bodyParser = require( 'body-parser' );
 const path = require( 'path' );
-
 const keys = require( './config/keys' );
+
 require( './services/passportServ' )( passport );
 
 app.use( bodyParser.json() );
@@ -18,17 +18,17 @@ app.use( passport.session() );
 
 require( './routes' )( app, passport );
 
-// if ( process.env.NODE_ENV === 'production' ) {
-//   console.log( 'IN PRODUCTION MODE!' );
+if ( process.env.NODE_ENV === 'production' ) {
+  console.log( 'IN PRODUCTION MODE!' );
 
-//   // Serve up production assets from build folder
-//   app.use( express.static( path.join( __dirname, '/client/build' ) ) );
+  // Serve up production assets from build folder
+  app.use( express.static( path.join( __dirname, '/client/build' ) ) );
 
-//   // Send index.html when route is not recognized
-//   app.get( '*', ( req, res ) => {
-//     res.sendFile( path.resolve( __dirname, 'client', 'build', 'index.html' ) );
-//   } );
-// }
+  // Send index.html when route is not recognized
+  app.get( '*', ( req, res ) => {
+    res.sendFile( path.resolve( __dirname, 'client', 'build', 'index.html' ) );
+  } );
+}
 
 const PORT = process.env.PORT || 5000;
 console.log( 'Listening on port ', PORT );
